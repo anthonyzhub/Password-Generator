c6c2fbb5cd7e2a60cbfe339cbbcbac804912c2b5
Anthony <zamora.anthony1234@gmail.com>
2019-11-23 18:04:22 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+import Generator
+
+if __name__ == "__main__":
+
+    # Create instance of Generator class
+    generator_class = Generator()
+
+    while True:
+
+        # Print options to user
+        print("1. Generate Password\n")
+        print("2. View Stored File\n")
+
+        # Ask for selection
+        option_selected = input("Enter Option:")
+
