2cd860717bf3ca996f8d1d911a4a2ad2794a4838
Anthony Zamora <zamora.anthony1234@gmail.com>
2019-11-28 17:49:48 -0500

    Program is now run as command, instead of ordinary program. OTP class was replaced as Cryptography_Class, but serves the same purpose. Cryptography_class has been filled in as well, but missing a few functions.

--- Generator.py
+++ Generator.py
@@ -1,8 +1,158 @@
-import random
+import secrets
+import string
+import os
 
-class OTP(object):
+class Cryptography_Class(object):
 
     def __init__(self):
+
+        # full_keyboard_list[] is only implemented because this class only focuses on encrypting the message
+        self.full_keyboard_list = "1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()_+|}{;?><,./`'"
+
+    def generate_one_time_pad_sheets(self, num_of_sheets, sheets_length):
+
+        # OBJECTIVE: Generate OTP sheets to be used as references later on
+
+        print("Generating OTP sheets...")
+
+        # Create new files
+        for sheet in range(num_of_sheets):
+            with open("OTP-SHEET__" + str(sheet) + ".txt", "w") as sheet:
+
+                # Write random numbers to file
+                for _ in range(sheets_length):
+                    sheet.write(str(secrets.randbelow(82)) + "\n")
+
+        print("OTP sheets have been generated!")
+
+    def load_one_time_pad_sheet(self, otp_sheet):
+
+        # OBJECTIVE: Open a one time pad sheet for reading
+
+        print("Loading OTP sheet...")
+
+        # Open OTP sheet
+        with open("OTP-SHEET__" + otp_sheet + ".txt", "r") as otp_to_read:
+
+            # 'splitlines() breaks each line up into a single
+            # item in the list and removes '\n' character
+            message = otp_to_read.read().splitlines()
+
+        # If "message" is not NULL, return it
+        if message is not None:
+            print("Loaded OTP sheet!")
+            return message
+        else:
+            print("UNLOADED OTP sheet")
+
+    def read_users_file(self, filename):
+
+        # OBJECTIVE: Opens any file that the user chooses
+
+        print("Reading {} file...".format(filename))
+
+        # Open file with read-only
+        with open(filename, 'r') as f:
+            contents = f.read()
+
+        print("Finished reading.\n*whisphers* About time! {} was too long! *whisphers*\n Oh wait, is this thing on?".format(filename))
+
+        return contents
+
+    def write_users_file(self, filename, data):
+
+        # OBJECTIVE: Create a file and write user's content to it
+
+        print("Writing file as {}".format(filename))
+
+        # Open file with write-only
+        with open(filename, 'w') as f:
+            f.write(data)
+
+        # Print location of recently written file
+        print("{} written at {}".format(filename, os.getcwd()))
+        print("Finished writing. \n*Cracks binary knockles*")
+
+    def encrypt_file(self, plain_text, otp_sheet):
+
+        # OBJECTIVE: Encrypt any plain message with an OTP sheet
+
+        # Create empty string variable for further use
+        current_cipher_text = ""
+
+        print("Encrypting {}...".format(plain_text))
+
+        # enumerate() returns a character from plaintext and its position
+        for position, character in enumerate(plain_text):
+
+            # If character isn't in list, add it to current_cipher_text
+            if character not in self.full_keyboard_list:
+                current_cipher_text += character
+            else:
+                # Get position of the character with sheet
+                # Break down:
+                # 1. self.full_keyboard_list.index(character) -> gets position of the character inside list
+                # 2. int(otp_sheet[position]) -> otp_sheet[position] returns character at index
+                # 3. Steps #1 and #2 are added, so result is the outcome of shifting from a previous character
+                # 4. %81 -> self.full_keyboard_list has a totaly of 81 elements. A modulu is needed if the message is greater
+                # than 81 characters, so some characters will be reused
+
+                encrypted_character_result = (self.full_keyboard_list.index(character) + int(otp_sheet[position])) % 81
+
+                # Change number to letter
+                current_cipher_text += self.full_keyboard_list[encrypted_character_result]
+
+        if current_cipher_text is not None:
+            print("Encryption is complete!")
+            return current_cipher_text
+        else:
+            print("INCOMPLETE encryption")
+
+    def decrypt_file(self, cipher_text, otp_sheet):
+
+        # OBJECTIVE: Decrypt any ciphertext with an OTP sheet
+
+        # Create empty string for further use
+        plaintext = ""
+
+        print("Decrypting {}".format(cipher_text))
+
+        # enumerate() returns a character from plaintext and its position
+        for position, character in enumerate(cipher_text):
+
+            # Same logic as above
+            if character not in self.full_keyboard_list:
+                plaintext += character
+            else:
+                decrypted = (self.full_keyboard_list.index(character) - int(otp_sheet[position])) % 81
+                plaintext += self.full_keyboard_list[decrypted]
+
+        if plaintext is not None:
+            print("Successful decryption!")
+            return plaintext
+        else:
+            print("UNSUCCESSFUL decryption")
+
+    def decrypt_password_file(self):
+        
+        # OBJECTIVE: Decrypt encrypted password file and save it as another file
+
+        # Send password file to decrypt_file()
+        otp_for_decrypting = input("Which One-Time-Pad (OTP) should be used? (enter # only) ")
+        unencrypted_content = self.decrypt_file("password_file_encrypted.txt", otp_for_decrypting)
+
+        # Send unencrypted_content to write_users_file()
+        self.write_users_file("Unencrypted_Password_file.txt", unencrypted_content)
+
+    def write_password_file(self, new_account_credentials):
+        
+        # OBJECTIVE: Write to password file and encrypt it later on
+        pass
+
+    def menu_options(self):
+
+        # OBJECTIVE: Main interface for user to encrypt/decrypt files
+
         pass
 
 
@@ -14,8 +164,8 @@ class Generator(object):
         # Lists for password composition
         self.numeric_list = "1234567890"
         self.alpha_list = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-        self.special_characters_list = "~!@#$%^&*()_+|}{;?><,./`"
-        self.full_keyboard_list = "1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()_+|}{;?><,./`"
+        self.special_characters_list = "~!@#$%^&*()_+|}{;?><,./`'"
+        self.full_keyboard_list = "1234567890AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()_+|}{;?><,./`'"
 
     def random_pin_generator(self, pin_quantity, pin_length):
 
@@ -29,7 +179,8 @@ class Generator(object):
 
             # Execute for-loop based on pin_length
             for _ in range(pin_length):
-                pin_suggestion += random.choice(self.numeric_list)
+                # pin_suggestion += random.choice(self.numeric_list)
+                pin_suggestion += secrets.choice(self.numeric_list)
 
             # Print suggestion
             print("{} - {}".format(i+1, pin_suggestion))
@@ -46,7 +197,8 @@ class Generator(object):
 
             # Execute for-loop based on pin_length
             for _ in range(alpha_length):
-                alpha_suggestion += random.choice(self.alpha_list)
+                # alpha_suggestion += random.choice(self.alpha_list)
+                alpha_suggestion += secrets.choice(self.alpha_list)
 
             # Print suggestion
             print("{} - {}".format(i+1, alpha_suggestion))
@@ -63,7 +215,8 @@ class Generator(object):
 
             # Execute for-loop based on pin_length
             for _ in range(mix_length):
-                mix_suggestion += random.choice(self.full_keyboard_list)
+                # mix_suggestion += random.choice(self.full_keyboard_list)
+                mix_suggestion += secrets.choice(self.full_keyboard_list)
 
             # Print suggestion
             print("{} - {}".format(i+1, mix_suggestion))
@@ -73,9 +226,10 @@ class Generator(object):
         # OBJECTIVE: Handle user's action and calls any function N times
 
         # Ask for quantity of recommendations and password's quantity
-        password_type = input("Which kind of password? (Pin/Alpha/Mix) ")
+        password_type = input("Password type? (Pin/Alpha/Mix) ")
 
-        if not (password_type in ["Pin", "Alpha", "Mix"]):
+        # If user's selection is not available, then don't continue
+        if  password_type not in ["Pin", "Alpha", "Mix"]:
             print("{} is an invalid request!".format(password_type))
             return None
         
--- Main.py
+++ Main.py
@@ -1,22 +1,32 @@
+import sys
 from Generator import Generator
+from Generator import Cryptography_Class
 
 if __name__ == "__main__":
 
-    # Create instance of Generator class
+    # Create instance of all imported classes
     generator_class = Generator()
+    crypto_class = Cryptography_Class()
 
-    while True:
+    # Accept Terminal commands
+    arguments = sys.argv
 
-        # Print options to user
-        print("0. Exit")
-        print("1. Generate Password")
-        print("2. View Saved Passwords")
+    if len(arguments) != 2:
 
-        # Ask for selection
-        option_selected = input("Enter Option: ")
+        # Print message and stop program
+        # mode -> gp (generate password), vp (view passwords)
+        # file -> Text file to write in or read from
+        print("Usuage: psswd_gen <mode> [file]")
+        exit(0)
 
-        # Check if user wants to terminate program
-        if option_selected == "0":
-            exit(1)
-        elif option_selected == "1":
-            generator_class.password_request()
+    # Execute password_request() with the following arguments
+    if (arguments[1] == "gp") or (arguments[1] == "Generate Password"):
+
+        # Show password generating options
+        generator_class.password_request()
+
+        # Ask user if password should be saved to an OTP file
+        save_file = input("Save a password to file? Y/n")
+        
+        if save_file == "Y" or save_file == "y":
+            crypto_class.menu_options()
